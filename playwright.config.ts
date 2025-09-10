6d9463812538c0c950b1721efc6848750e70efe4
Michael Mekhael <michael.mekhael@editel.sk>
2025-09-10 15:52:56 +0300

    add dotnet import

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from '@playwright/test';
+import 'dotenv/config';  
+
 
 export default defineConfig({
   use: {
-    baseURL: process.env.BASE_URL || 'https://stage.4shipper.transportly.eu',
+    baseURL: process.env.BASE_URL ?? 'https://stage.4shipper.transportly.eu',
     timezoneId: 'Europe/Prague',                 // keep date math stable
     testIdAttribute: 'data-test-id',             // optional; matches your DOM
     trace: 'retain-on-failure',
